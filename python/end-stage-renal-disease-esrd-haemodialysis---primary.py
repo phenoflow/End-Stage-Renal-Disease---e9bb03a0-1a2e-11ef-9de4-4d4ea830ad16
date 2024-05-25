a9687d8cba7aa66e27c2a7b7011f0ba5142b5ce1
Martin Chapman <contact@martinchapman.co.uk>
2024-05-25 01:44:16 +0100

    Update made by Phenoflow

---
+++ python/end-stage-renal-disease-esrd-haemodialysis---primary.py
@@ -0,0 +1,25 @@
+# Kethryn E Mansfield, Dorothea Nitsch, Liam Smeeth, Krishnan Bhaskaram, Laurie A Tomlinson, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"7L1A000","system":"readv2"},{"code":"Z91A.00","system":"readv2"},{"code":"Gy51.00","system":"readv2"},{"code":"Z919100","system":"readv2"},{"code":"TA02000","system":"readv2"},{"code":"Gy41.00","system":"readv2"},{"code":"4I29.00","system":"readv2"},{"code":"7L1A200","system":"readv2"},{"code":"14V2.00","system":"readv2"},{"code":"Z919300","system":"readv2"},{"code":"7L1A400","system":"readv2"},{"code":"14V2.11","system":"readv2"},{"code":"7L1B200","system":"readv2"},{"code":"SP0G.00","system":"readv2"},{"code":"7L1A.11","system":"readv2"},{"code":"7L1A600","system":"readv2"},{"code":"ZV45100","system":"readv2"},{"code":"7L1A100","system":"readv2"},{"code":"ZVu3G00","system":"readv2"},{"code":"Z99.2","system":"readv2"},{"code":"Y84.1","system":"readv2"},{"code":"Z49.2","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('end-stage-renal-disease-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["end-stage-renal-disease-esrd-haemodialysis---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["end-stage-renal-disease-esrd-haemodialysis---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["end-stage-renal-disease-esrd-haemodialysis---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
